96a8b3171d8a8fcfa5f6bd8d55aa505953b0ab2b
ertepro <diego056790@gmail.com>
2025-11-05 18:24:32 +0100

    made it work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 
 import './App.css';
-import SearchBar from './searchBar';
 import Footer from "./Footer";
 import {useEffect, useState} from "react";
-import searchBar from "./searchBar";
-function App() {
+import {useForm} from "react-hook-form";
 
-    let string ="https://api.tvmaze.com/search/shows?q="+SearchBar.search;
+function App() {
+    const{register, handleSubmit, watch} = useForm();
+    const onSubmit = (data, e) => console.log(data);
+    const onError = (errors, e) => console.log(errors, e);
+    const searchValue = watch("search");
     const [data,setData]=useState([]);
+
     useEffect(()=>{
-        fetch(string)
+
+        fetch("https://api.tvmaze.com/search/shows?q="+searchValue)
             .then((res) => res.json())
             .then(data => setData(data))
-    },[string])
+    },[data, searchValue, setData]);
+    console.log(data)
 
-    console.log(data);
-    console.log(string)
+
+
 
 
 
@@ -25,10 +30,21 @@ function App() {
         <div className="header">
         <h1>TVMAZE</h1>
         </div>
-        <SearchBar></SearchBar>
+        <form className="searchbar" onSubmit={handleSubmit(onSubmit,onError)}>
+            <input {...register("search")}></input>
+            <button type="submit">Search</button>
+        </form>
+        {data.map((item)=>(
+            <div className="item" key={item.id}>
+            <p key={item.show.id}> {item.show.name} type: {item.show.type}</p>
+                <img className="img" src={item.show.image} alt=""/>
+            </div>))}
         <Footer></Footer>
 
-        </>
+</>
+
+
+
   );
 }
 
